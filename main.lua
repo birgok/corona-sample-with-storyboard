784fca1863c5092dedc22152a25f869e1ac6e51a
Fabio Hisamoto <fabio@bestcoolfungames.com>
2012-07-12 14:34:25 -0300

    Added actions to buttons and added RevMob banner.

--- main.lua
+++ main.lua
@@ -13,21 +13,52 @@ local widget = require "widget"
 -- load first screen
 storyboard.gotoScene( "scene1" )
 
-
 -- Display objects added below will not respond to storyboard transitions
+require 'revmob'
+local params =  {
+  ["Android"] = "4f56aa6e3dc441000e005a20",
+  ["iPhone OS"] = "4fd619388d314b0008000213",
+  x = display.contentWidth / 2,
+  y = display.contentHeight - 20,
+  width = 300,
+  height = 40
+}
+local banner = RevMob.createBanner(params)
 
 -- table to setup tabBar buttons
 local tabButtons = {
-	{ label="First", up="icon1.png", down="icon1-down.png", width = 32, height = 32, selected=true },
-	{ label="Second", up="icon2.png", down="icon2-down.png", width = 32, height = 32 },
+  {
+    label="Show fullscreen", up="icon1.png", down="icon1-down.png", width = 32, height = 32,
+    onPress = function(event)
+      timer.performWithDelay(100, function()
+        RevMob.showFullscreen(params)
+      end)
+      return true
+    end
+	},
+
+	{
+	  label = "Show pop up", up = "icon1.png", down = "icon1-down.png", width = 32, height = 32,
+	  onPress = function(event)
+      RevMob.showPopup(params)
+      return true
+	  end
+	},
+
+	{
+	  label = "Open link", up = "icon1.png", down = "icon1-down.png", width = 32, height = 32,
+	  onPress = function(event)
+      RevMob.openAdLink(params)
+      return true
+	  end
+	},
 }
 
 -- create the actual tabBar widget
 local tabBar = widget.newTabBar{
-	top = display.contentHeight - 50,	-- 50 is default height for tabBar widget
-	buttons = tabButtons
+ top = 0, -- 50 is default height for tabBar widget
+ buttons = tabButtons
 }
-
 
 --[[ Uncomment to monitor app's lua memory/texture memory usage in terminal...
 
--- scene3.lua
+++ scene3.lua
@@ -15,9 +15,9 @@ local image, text1, text2, text3
 
 local function onSceneTouch( self, event )
 	if event.phase == "began" then
-		
+
 		storyboard.gotoScene( "scene4", "fromLeft", 800  )
-		
+
 		return true
 	end
 end
@@ -26,42 +26,42 @@ end
 -- Called when the scene's view does not exist:
 function scene:createScene( event )
 	local screenGroup = self.view
-	
+
 	image = display.newImage( "bg3.jpg" )
 	screenGroup:insert( image )
-	
+
 	image.touch = onSceneTouch
-	
+
 	text1 = display.newText( "Scene 3", 0, 0, native.systemFontBold, 24 )
 	text1:setTextColor( 255 )
 	text1:setReferencePoint( display.CenterReferencePoint )
-	text1.x, text1.y = display.contentWidth * 0.5, 50
+	text1.x, text1.y = display.contentWidth * 0.5, 100
 	screenGroup:insert( text1 )
-	
+
 	text2 = display.newText( "MemUsage: ", 0, 0, native.systemFont, 16 )
 	text2:setTextColor( 255 )
 	text2:setReferencePoint( display.CenterReferencePoint )
 	text2.x, text2.y = display.contentWidth * 0.5, display.contentHeight * 0.5
 	screenGroup:insert( text2 )
-	
+
 	text3 = display.newText( "Touch to continue.", 0, 0, native.systemFontBold, 18 )
 	text3:setTextColor( 255 ); text3.isVisible = false
 	text3:setReferencePoint( display.CenterReferencePoint )
 	text3.x, text3.y = display.contentWidth * 0.5, display.contentHeight - 100
 	screenGroup:insert( text3 )
-	
+
 	print( "\n3: createScene event" )
 end
 
 
 -- Called immediately after scene has moved onscreen:
 function scene:enterScene( event )
-	
+
 	print( "3: enterScene event" )
-	
+
 	-- remove previous scene's view
 	storyboard.purgeScene( "scene2" )
-	
+
 	-- update Lua memory text display
 	local showMem = function()
 		image:addEventListener( "touch", image )
@@ -75,12 +75,12 @@ end
 
 -- Called when scene is about to move offscreen:
 function scene:exitScene()
-	
+
 	print( "3: exitScene event" )
-	
+
 	-- remove touch listener for image
 	image:removeEventListener( "touch", image )
-	
+
 	-- reset label text
 	text2.text = "MemUsage: "
 end
@@ -88,7 +88,7 @@ end
 
 -- Called prior to the removal of scene's "view" (display group)
 function scene:destroyScene( event )
-	
+
 	print( "((destroying scene 3's view))" )
 end
 
